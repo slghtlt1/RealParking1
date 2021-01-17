9ce053a93453594b4356ad9597f7238da8f186a1
slghtlt1 <auto.it.email@gmail.com>
2021-01-17 23:22:14 +0200

    Printed the ticket

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.xml.transform.Result;
+import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
@@ -11,24 +9,18 @@ public class Main {
     public static void main(String arg[]) throws ClassNotFoundException {
         Scanner s = new Scanner(System.in);
         try {
-            String url = "jdbc:mysql://localhost:3306/AutonomParking";
-            Connection conn = DriverManager.getConnection(url, "root", "slghtlt1");
-            Class.forName("com.mysql.jdbc.Driver");
-            Statement st = conn.createStatement();
+//            String url = "jdbc:mysql://localhost:3306/AutonomParking";
+//            Connection conn = DriverManager.getConnection(url, "root", "slghtlt1");
+//            Class.forName("com.mysql.jdbc.Driver");
+//            Statement st = conn.createStatement();
             System.out.println("Choose an option: ");
             int var = s.nextInt();
             switch (var) {
                 case 1:
-//                    System.out.println("Insert VehicleNr: ");
-//                    String vehNr = s.next();
-//                    SimpleDateFormat formatter= new SimpleDateFormat("dd.MM.yy");
-//                    Date date = new Date(System.currentTimeMillis());
-//                    System.out.println(formatter.format(date));
-//                    st.executeUpdate("Insert INTO InforTichet "+"VALUES(default, '"+vehNr+"', '"+formatter.format(date)+"')");
-                    System.out.println("Do you wanna check available nr. of the par");
+                    insertData();
                     break;
                 case 2:
-                    System.out.println("Your tichet");
+                    printData();
                     break;
                 case 3:
                     System.out.println("Payment area");
@@ -42,4 +34,49 @@ public class Main {
             System.out.println(e.getMessage());
         }
     }
+
+    static void insertData() throws SQLException, ClassNotFoundException {
+        Scanner s = new Scanner(System.in);
+
+        String url = "jdbc:mysql://localhost:3306/AutonomParking";
+        Connection conn = DriverManager.getConnection(url, "root", "slghtlt1");
+        Class.forName("com.mysql.jdbc.Driver");
+        Statement st = conn.createStatement();
+
+        System.out.println("Insert VehicleNr: ");
+
+        String vehNr = s.next();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yy HH:mm:ss");
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println(formatter.format(date));
+
+        st.executeUpdate("Insert INTO InforTichet " + "VALUES(default, '" + vehNr + "', '" + formatter.format(date) + "')");
+    }
+
+    static void printData() throws ClassNotFoundException, SQLException {
+        Scanner s = new Scanner(System.in);
+
+        String url = "jdbc:mysql://localhost:3306/AutonomParking";
+        Connection conn = DriverManager.getConnection(url, "root", "slghtlt1");
+        Class.forName("com.mysql.jdbc.Driver");
+        Statement st = conn.createStatement();
+
+        System.out.println("Insert your tichet nr: ");
+        int tichetID = s.nextInt();
+
+        String sql = "Select IDTichet, NrInmatriculare, OraIntrare from InforTichet";
+        ResultSet rs = st.executeQuery(sql);
+
+        while (rs.next()) {
+            int id = rs.getInt("IDTichet");
+            String nrInmatriculare = rs.getString("NrInmatriculare");
+            Date dateEntry = rs.getDate("OraIntrare");
+
+            if (id == tichetID)
+                System.out.println("ID with nr " + id + ", and Nr registration " + nrInmatriculare + ", you entered at this hour:" + dateEntry);
+            else break;
+        }
+        System.out.println("Enter a valid option, please repet");
+    }
+
 }
